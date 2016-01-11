7d344cc4a236cffb7f593bd72d186a3d66f86e53
Bernardo Lins <bernardolins28@gmail.com>
2016-01-11 01:15:22 -0200

    Add spotligth and sphere on scene

--- introduction/js/introduction.js
+++ introduction/js/introduction.js
@@ -13,15 +13,38 @@ window.Introduction = {
     } 
   },
 
-  initialize: function(selector) {
-    var $selector = $(selector); 
+  sphereOptions: {
+    radius: 50,
+    segments: 15,
+    rings: 15,
+    material: new THREE.MeshLambertMaterial({
+      color: 0xCC0000 
+    }) 
+  },
 
+  initialize: function(selector) {
     var renderer = new Renderer(this.aspect);
     var camera = new Camera(this.camera.angle, this.camera.position, this.aspect);
     var scene = new Scene(camera);
 
-    $selector.append(renderer.domElement);
+    var sphere = new Sphere(this.sphereOptions.radius, this.sphereOptions);
+
+    var pointLight = new THREE.PointLight( 0xFFFFFF );
+
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+
+    scene.add(sphere); 
+    scene.add(camera); 
+    scene.add(pointLight);
+
+    renderer.render(scene, camera);
+
+    document.getElementById(selector).appendChild(renderer.domElement);
   }
 };
 
-Introduction.initialize(".renderArea");
+window.onload = function() {
+  Introduction.initialize("renderArea");
+}
