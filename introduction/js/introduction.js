91122d6f7ba922717e51412cf461d7cdb546dc2e
Bernardo Lins <bernardolins28@gmail.com>
2016-01-10 23:52:15 -0200

    Add initialization of threejs scene

--- introduction/js/introduction.js
+++ introduction/js/introduction.js
@@ -1,1 +1,27 @@
-window.Introduction = {}
+window.Introduction = {
+  aspect: {
+    width: 500,
+    height: 500  
+  },
+
+  camera: {
+    angle: 45,
+    position: {
+      x: 0,
+      y: 0,
+      z: 300 
+    } 
+  },
+
+  initialize: function(selector) {
+    var $selector = $(selector); 
+
+    var renderer = new Renderer(this.aspect);
+    var camera = new Camera(this.camera.angle, this.camera.position, this.aspect);
+    var scene = new Scene(camera);
+
+    $selector.append(renderer.domElement);
+  }
+};
+
+Introduction.initialize(".renderArea");
