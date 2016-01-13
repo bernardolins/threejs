4450f8bcdc33265dbfe95a05bc9d7a4fba3edafd
bernardo.lins <bernardo.lins@corp.globo.com>
2016-01-13 13:39:34 -0200

    Add Earth to scene

--- solar/js/solar.js
+++ solar/js/solar.js
@@ -1,11 +1,11 @@
 window.Solar = {
   aspect: {
-    width: 500,
-    height: 500  
+    width: 1000,
+    height: 1000  
   },
 
   camera: {
-    angle: 45,
+    angle: 75,
     position: {
       x: 0,
       y: 0,
@@ -14,6 +14,7 @@ window.Solar = {
   },
 
   earthOptions: {
+    texture: "img/earth.jpg",
     radius: 50,
     segments: 15,
     rings: 15
@@ -28,30 +29,37 @@ window.Solar = {
     color: 0xFFFFFF 
   },
 
-  initialize: function() {
+  selector: "renderArea",
+
+  initializeScene: function() {
     this.renderer = new Renderer(this.aspect);
     this.camera = new Camera(this.camera.angle, this.camera.position, this.aspect);
     this.scene = new Scene(this.camera);
-
-    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    
+    var ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white ambientLight
+    var pointLight = new PointLight(this.light.position, this.light.color);
       
-    var material  = new THREE.MeshPhongMaterial({color: 0xCC0000});
+    var earth = new Planet(this.earthOptions, function() {
+      Solar.render();
+    });
 
-    var sphere = new Sphere(material, this.earthOptions);
+    var starfield = new StarField(function() {
+      Solar.render();
+    });
 
-    this.scene.add(light);
-    this.scene.add(sphere);
+    this.scene.add(ambientLight);
+    this.scene.add(pointLight);
+    this.scene.add(earth);
+    this.scene.add(starfield);
     this.scene.add(this.camera); 
-
   },
-
-  render: function(selector) {
+  
+  render: function() {
     this.renderer.render(this.scene, this.camera); 
-    document.getElementById(selector).appendChild(this.renderer.domElement);
+    document.getElementById(this.selector).appendChild(this.renderer.domElement);
   }
 };
 
 window.onload = function() {
-  Solar.initialize();
-  Solar.render("renderArea");
+  Solar.initializeScene();
 }
